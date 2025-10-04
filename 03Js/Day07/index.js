1d8a484661655584ac06ff52448836656c0bd313
annytrix <kumarianjana5801@gmail.com>
2025-10-04 23:57:26 +0530

    array

---
+++ 03Js/Day07/index.js
@@ -0,0 +1,48 @@
+// Array;
+
+// let marks1 = 100;
+// let marks2 = 70;
+// let marks3 = 80;
+// let marks4 = 50;
+
+let marks =[100,70,80,50,90];
+
+
+
+console.log(marks);
+console.log(marks.length);
+
+let arr=[30,20,"Anny",true];
+
+// console.log(arr[2]);
+// console.log(typeof arr);
+
+// arr[1]=90;
+// console.log(arr);
+
+
+// push:Insert element at End
+
+// arr.push(90);
+// arr.push("strike");
+
+// console.log(arr);
+
+// pop opration:Delete  element from End
+
+// arr.pop();
+
+// console.log(arr);
+// Starting me add kr sakta hu, or Delete bhi kr sakta hu
+
+// arr.unshift(10);
+// arr.unshift(40);
+// console.log(arr);
+
+// Delete
+
+// arr.shift();
+// console.log(arr);
+
+
+
