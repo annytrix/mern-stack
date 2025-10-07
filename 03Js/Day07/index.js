7cf74d5cf67fc802f7668d2620853e702b919d45
annytrix <kumarianjana5801@gmail.com>
2025-10-07 22:59:07 +0530

    hello

--- 03Js/Day07/index.js
+++ 03Js/Day07/index.js
@@ -5,14 +5,14 @@
 // let marks3 = 80;
 // let marks4 = 50;
 
-let marks =[100,70,80,50,90];
+// let marks =[100,70,80,50,90];
 
 
 
-console.log(marks);
-console.log(marks.length);
+// console.log(marks);
+// console.log(marks.length);
 
-let arr=[30,20,"Anny",true];
+// let arr = [30,20,"Anny",true];
 
 // console.log(arr[2]);
 // console.log(typeof arr);
@@ -44,5 +44,101 @@ let arr=[30,20,"Anny",true];
 // arr.shift();
 // console.log(arr);
 
+// let  arr = [100,70,80,90,40];
+
+// for(let i=0;i<arr.length;i++){
+
+//     console.log(arr[i]);
+// }
+
+// for(let num of arr){
+
+// console.log(num);}
+
+//  let arr =[100,70,80,90,40];
+
+//   let arr2 = arr;
+
+//   arr.push(30);
+//   console.log(arr);
+// Object(non primitive): Copy by reference hote hai
+// Primitive: Copy by value
+
+// const arr = [10,30,50,90,11];
+// arr[2] = 30;
+
+
+
+// console.log(arr);
+
+
+// const arr = [10,30,50,90,11];
+// const arr2 = arr.slice(2,4);
+// console.log(arr);
+// const arr3 = arr.splice(1,3,"Rohit",19);
+// console.log(arr3);
+
+// const arr = [10,30,50,90,11];
+// const arr2 = ["Rohit",11,true];
+// const arr4 = [90,4,false];
+
+// arr.push(arr2);
+// const arr3 = arr.concat(arr2,arr4);
+// const arr3 = [arr,arr2,arr4];
+
+// spread operator
+
+// const arr3= [...arr,...arr2,...arr4];
+// console.log(arr3);
+
+
+// const names = ["Alice", "Rohit","Bob","Mohit", "Charlie",];
+
+// console.log(names.toString());
+// console.log(names.join("-"));
+// console.log(names.lastIndexOf("Bob"));
+// console.log(names.includes("Bobs"));
+
+// names.sort();
+// names.reverse();
+
+// console.log(names);
+
+// const a = ["101","90","80","32","91"];
+// const arr = [10,"Rohit","Mohan",true];
+
+// arr.sort();
+// console.log(arr);
+
+// const arr = [10,40,31,71,5,11];
+
+// arr.sort((a,b)=> a-b);
+// // ascending order
+
+// arr.sort((a,b)=> b-a);
+
+// 40,10
+// a = 10
+// b = 40
+// 10,31, 40
+// a = 10
+// b = 31
+
+// -ve : pehle a aayega fr aayega
+// +ve: b aayega fr a aayge
+
+// console.log(arr);
+
+
+// const arr = [10,30,50,[40,90,[60,19,99],11], 80];
+
+// const a = arr.flat(Infinity);
+// console.log(a);
+// console.log(arr[3][2][1]);
+
+
+const a = [10,309,"Anny",9.3, true];
+a.name = "Moahn";
+console.log(a);
 
 
---
+++ 03Js/Day08/index.js
@@ -0,0 +1,177 @@
+// object
+// key value
+// const user = {
+//    name: "Anny",
+//    age: 20,
+//    emailId: "anny@gmail.com",
+//    amount: 2600,
+//    "home address": "Delhi"
+// }
+
+
+// console.log(user["name"]);
+// console.log(user.age);
+// console.log(user["age"]);
+// console.log(user["home address"]);
+// console.log(typeof user);
+// CRUD operation: Create read update delete
+
+
+// console.log(user.age);
+
+//  update
+// user.aadhar = 1234;
+// user.amount = 5000;
+
+// console.log(user);
+
+// // delete
+// delete user.emailId;
+// console.log(user);
+
+
+// const user = {
+//    name: "Anny",
+//    age: 20,
+//    emailId: "Anny@gmail.com",
+//    amount: 2600,
+// }
+
+// const user2 = user;
+// user2.age = 90;
+
+// console.log(user);
+
+// Important
+
+// console.log(Object.keys(user));
+// console.log(Object.values(user));
+// console.log(Object.entries(user));
+
+// for(let keys in user){
+//     console.log(keys, user[keys]); }
+
+// user.keys user["name"] user["age"]
+
+
+// const user = {
+//    name: "Anny",
+//    age: 20,
+//    emailId: "Anny@gmail.com",
+//    amount: 2600,}
+
+// for of loop array ke
+// const temnparr = Object.keys(user);
+// console.log(temnparr)
+// for(let keys of temnparr){
+//     console.log(keys);
+// }
+
+// for(let values of Object.values(user)){
+//     console.log(values);
+// }
+
+// console.log(Object.entries(user));
+// [
+//   [ 'name', 'Anny' ],
+//   [ 'age', 20 ],
+//   [ 'emailId', 'Anny@gmail.com' ],
+//   [ 'amount', 2600 ]
+// ]
+
+// for(let [keys,values] of Object.entries(user)){
+//     console.log(keys, values);
+
+// }
+
+
+// const name = user.name;
+// const age = user.age;
+
+// object ko destructing karna
+// const {name:userName,age:userAge} = user;
+
+// const arr = [10,20,40,80,11];
+
+// const [first,second]= arr;
+
+// console.log(userName,userAge);
+// console.log(first,second);
+
+
+// const user = {
+//    name: "Anny",
+//    age: 20,
+//    emailId: "anny@gmail.com",
+//    amount: 2600,
+//    greeting: function(){
+//     console.log(`hell every kaise ho ${this.name}`);
+//     return 20;
+//    }
+// }
+
+// const user2 = {
+//     name:"Mohan",
+//     account:201,
+//      greeting: function(){
+//     console.log(`hello everyone kaise ho ${this.name}`);
+//     return 20;
+//    }
+// }
+
+// user2.greeting = user.greeting;
+
+// user2.greeting();
+// const va = user.greeting();
+// console.log(va);
+
+// nested object
+
+// const user = {
+//    name: "Anny",
+//    age: 20,
+//    emailId: "anny@gmail.com",
+//    amount: 2600,
+//    address: {
+//     city:"rajiw chowk",
+//     state:"delhi"
+//    }
+// }
+
+// shallow copy
+// const user2 = {...user};
+// user2.name = "Mohan";
+// user2.address.city = "Delhi";
+
+// console.log(user2);
+
+// console.log(user.address.city);
+
+
+// deep copy
+
+// const user2 = structuredClone(user);
+// user2.address.city = "Dwarka";
+// console.log(user);
+// keY; String || Symbol
+
+// const sym = Symbol("id");
+
+// const user = {
+//     name:"Anny",
+//     age:20,
+//     0: 100,
+//     2: "Mohan",
+//     [sym]:"Hii Ji"
+// }
+
+// const arr = [10,20,30,40];
+
+// {
+//     0:10,
+//     1:20,
+//     2:30,
+//     3: 40
+// }
+
+console.log(user[sym]);
