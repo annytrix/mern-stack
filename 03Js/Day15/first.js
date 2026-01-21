e1d959b6c8bc8b4c9e805b155053906e1f958e88
annytrix <kumarianjana5801@gmail.com>
2026-01-21 20:37:40 +0530

    evv

--- 03Js/Day15/first.js
+++ 03Js/Day15/first.js
@@ -1,13 +1,22 @@
+
+
 // function handleClick(){
 //     const element = document.getElementById("first");
 //      element.textContent = "Hii Anny";
 // }
 
-const element = document.getElementById("first");
-element.onclick = function handleClick(){
-element.textContent = "Hii Anny";
-}
+ const element = document.getElementById("first");
+// element.onclick = function handleClick(){
+// element.textContent = "Hii Anny";
+// }
 
-element.onclick = function handleClick(){
-  element.textContent = "yes plz";
-}
+// element.onclick = function handleClick(){
+//   element.textContent = "yes plz";
+ 
+element.addEventListener('click',()=>{
+   element.textContent="helo Anny";
+  })
+
+element.addEventListener('click',()=>{
+    element.style.backgroundColor="yellow";
+  })
