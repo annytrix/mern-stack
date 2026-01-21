629ac76b05bc31a3034cc27eeb4289fdd09c6845
annytrix <kumarianjana5801@gmail.com>
2026-01-22 00:31:50 +0530

    ent

--- 03Js/Day15/first.js
+++ 03Js/Day15/first.js
@@ -5,7 +5,7 @@
 //      element.textContent = "Hii Anny";
 // }
 
- const element = document.getElementById("first");
+//  const element = document.getElementById("first");
 // element.onclick = function handleClick(){
 // element.textContent = "Hii Anny";
 // }
@@ -13,10 +13,25 @@
 // element.onclick = function handleClick(){
 //   element.textContent = "yes plz";
  
-element.addEventListener('click',()=>{
-   element.textContent="helo Anny";
-  })
+// element.addEventListener('click',()=>{
+//    element.textContent="helo Anny";
+//   })
 
-element.addEventListener('click',()=>{
-    element.style.backgroundColor="yellow";
-  })
+// element.addEventListener('click',()=>{
+//     element.style.backgroundColor="yellow";
+//   })
+
+//  const child1 = document.getElementById("child1");
+//  child1.addEventListener('click',()=>{
+//   child1.textContent = "you are clicked";
+//  })
+
+  const parent = document.getElementById("parent");
+  console.log(parent.children);
+
+  for (let child of parent.children){
+    console.log(child);
+    child.addEventListener('click',()=>{
+      child.textContent= "you are click";
+    })
+  }
