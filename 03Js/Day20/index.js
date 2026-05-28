f96ce7cfc772424cb4c740ee503c6af2e39dd7bb
annytrix <kumarianjana5801@gmail.com>
2026-05-29 03:00:25 +0530

    as

--- 03Js/Day20/index.js
+++ 03Js/Day20/index.js
@@ -1,18 +1,24 @@
 //async await
 
-async function greet(){
-    return "Anny singh";
+//async function greet(){
+   // return "Anny singh";
 
 
-return new Promise((resolve ,reject)=>{
-    reject("Anny singh");
-})
-}
-const response = greet();
+//return new Promise((resolve ,reject)=>{
+  //  reject("Anny singh");
+//})
+//}
+//const response = greet();
 //console.log(response);
-response.then((data)=>console.log(data));
+//response.then((data)=>console.log(data));
 
  //.catch((error)=>{
     //console.log("Error:",error);
     //})
-    
+
+    // fetch("https://api.github.com/users")
+    // .then((response )=>response.json())
+    // .then((data)=>console.log(data));
+  const response=await fetch("https://api.github.com/users")
+  const data= await response.json();
+  console.log(data);
