06f6c09de6c6a4f99f15717e3b7a1a95d379f459
annytrix <kumarianjana5801@gmail.com>
2026-05-29 17:19:18 +0530

    project1

--- 03Js/Day20/index.js
+++ 03Js/Day20/index.js
@@ -12,13 +12,186 @@
 //console.log(response);
 //response.then((data)=>console.log(data));
 
- //.catch((error)=>{
+    //.catch((error)=>{
     //console.log("Error:",error);
     //})
 
     // fetch("https://api.github.com/users")
     // .then((response )=>response.json())
     // .then((data)=>console.log(data));
-  const response=await fetch("https://api.github.com/users")
-  const data= await response.json();
-  console.log(data);
+    
+
+  // async function github() {
+  //   try{
+  //   console.log("Hello Anny")
+  //   const  response = await fetch("https://api.github.com/users");
+  //     if(!response.ok){
+  //       throw new Error("Data is not resent");
+  //     }
+
+  //   const data= await response.json();
+  //   //console.log(data);
+
+  //   const parent = document.getElementById("first");
+
+  //   for(let user of data){
+       
+  //      const  Element = document.createElement("div");
+  //       Element.classList.add("user");
+
+  //       const image = document.createElement('img');
+  //       image.src = user.avatar_url;
+
+  //       const userName = document.createElement('h2');
+  //       userName.textContent = user.login;
+
+  //       const anchor = document.createElement('a');
+  //       anchor.href = user.html_url;
+  //       anchor.textContent ="Visit profile";
+
+  //       Element.append(image,userName,anchor);
+  //       parent.append(Element);
+
+  //   }
+  //   }
+  //   catch(error){
+  //     console.log("error");
+
+  //   }
+
+
+  // }
+
+
+  // github();
+
+
+  //console.log("Hello how are you");
+
+  const orderDetail = {
+    orderId: 123123,
+    food:["Pizza","biryani","coke"],
+    cost: 620,
+    customer_name: "Anny",
+    customer_location: "town",
+    restaurant_location: "Delhi"
+}
+
+function placedOrder(orderDetail){
+    console.log(`${orderDetail.cost} Payment is in progress`);
+    
+    return new Promise((resolve,reject)=>{
+       setTimeout(()=>{
+     
+      if(Math.random()>0.1){
+      console.log("Payment is received and order get placed");
+      orderDetail.status = true;
+       resolve(orderDetail);
+      }
+      else{
+        reject("Payment is failed");
+      }
+
+    },3000)
+    })
+   
+}
+
+
+
+function preparingOrder(orderDetail){
+    console.log(`Your food preparation started of ${orderDetail.food}`);
+    
+    return new Promise((resolve,reject)=>{
+  
+     setTimeout(()=>{
+
+        if(Math.random()>0.05){ 
+        console.log("Your order is now prepared");
+        orderDetail.token = 123;
+        resolve(orderDetail);
+        }
+        else{
+            reject("Food item is not persent at restaurant");
+        }
+        
+    },3000);
+    })
+    
+}
+
+
+function pickupOrder(orderDetail){
+    console.log(`Delivery boy is on way to pickup order from ${orderDetail.restaurant_location} `);
+    
+    return new Promise((resolve,reject)=>{
+
+
+        setTimeout(()=>{
+         if(Math.random()>0.05){   
+        console.log("I have picked up the order");
+        orderDetail.received = true;
+        resolve(orderDetail);
+        }
+        else{
+           reject("Delivery boy Unable to reach restaurant")
+        }
+    },3000);
+    })
+    
+}
+
+
+function deliverOrder(orderDetail){
+    console.log(`I am on my way to deliver order ${orderDetail.customer_location}`);
+    
+    return new Promise((resolve,reject)=>{
+       setTimeout(()=>{
+        console.log("Order delivered succesfully");
+        orderDetail.delivery = true;
+        resolve(orderDetail);
+    },3000)
+    })
+    
+}
+
+
+// placedOrder(orderDetail)
+// .then((orderDetail)=>preparingOrder(orderDetail))
+// .then((orderDetail)=>pickupOrder(orderDetail))
+// .then((orderDetail)=>deliverOrder(orderDetail))
+// .then((orderDetail)=>{
+//     console.log(orderDetail);
+// })
+// .catch((error)=>{
+//     console.log("Error: ", error);
+// }).
+// finally(()=>{
+//     console.log("I am doing cleanup");
+// })
+
+
+// async function ordering() {
+//   try{
+
+//     const response1 = await placedOrder(orderDetail);
+//     const response2 = await preparingOrder(response1);
+//     const response3 = await pickupOrder(response2);
+//     const response4 = await deliverOrder(response3);
+//     console.log(response4); 
+//   } 
+//   catch(error){
+//     console.log("Error:",error);
+//   }
+
+// }
+
+
+// ordering();
+async function userDetail(params) {
+//    const comment = await fetch("userComment");
+//    const photos =  await fetch("userphoto");
+//     const chat = await fetch("chat");
+
+const[comment,photo,chat] =await Promise.all([fetch("userComment"),fetch("photo"),fetch("chat")]);
+ }
